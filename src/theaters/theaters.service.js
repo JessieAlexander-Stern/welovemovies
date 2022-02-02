a9c8230b1948f47be1a1017b33e95f690e946671
Jessie Alexander-Stern <idahofiddler@gmail.com>
2022-02-01 18:37:13 -0700

    fixed missing

--- src/theaters/theaters.service.js
+++ src/theaters/theaters.service.js
@@ -8,17 +8,27 @@ const reduceMovies = reduceProperties("theater_id", {
   rating: ["movies", null, "rating"],
   description: ["movies", null, "description"],
   image_url: ["movies", null, "image_url"],
-  is_showing: ["movies", null, "is_showing"],
 });
 
-function list() {
+async function list() {
   return knex("theaters")
-    .join("movies_theaters as mt", "mt.theater_id", "theaters.theater_id")
-    .join("movies", "movies.movie_id", "mt.movie_id")
-    .select("*")
+    .join(
+      "movies_theaters",
+      "movies_theaters.theater_id",
+      "theaters.theater_id"
+    )
+    .join("movies", "movies.movie_id", "movies_theaters.movie_id")
     .then(reduceMovies);
+}
+
+function listTheatersByMovieId(movieId) {
+  return knex("theaters")
+    .join("movie_theaters", "theaters.theater_id", "movie_theaters.theater_id")
+    .select("theaters.*", "is_showing", "movie_id")
+    .where({ movie_id: movieId });
 }
 
 module.exports = {
   list,
+  listTheatersByMovieId,
 };
